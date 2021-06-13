68107de658ddd5e99df0424e7e189b725aabbf34
Takeshi Takatsudo <takazudo@gmail.com>
2021-06-13 16:49:03 +0900

    installed gatsby latest

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: "gatsby-aws-auth-test",
+  },
+  plugins: [],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return <div>Index</div>;
+};
+
+export default IndexPage;
