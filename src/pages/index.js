f66d4fa23dedb56c012d233f88528e77b90b0ff4
Takeshi Takatsudo <takazudo@gmail.com>
2021-06-14 15:47:56 +0900

    auth worked

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,53 @@
 import * as React from "react";
+import Amplify from 'aws-amplify';
+import { default as awsconfig } from '../aws-exports';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
-const IndexPage = () => {
-  return <div>Index</div>;
+// Amplifyの設定を行う
+Amplify.configure(awsconfig);
+
+// SingUp時に、メールアドレスとパスワードを要求する
+const signUpConfig = {
+  header: "Sign Up",
+  hideAllDefaults: true,
+  defaultCountyCode: "1",
+  signUpFields: [
+    {
+      label: "User Name",
+      key: "username",
+      required: true,
+      displayOrder: 1,
+      type: "string",
+    },
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 2,
+      type: "string",
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 3,
+      type: "password",
+    },
+  ],
 };
 
-export default IndexPage;
+//// SingOut
+//function signOut() {
+//  Auth.signOut().then().catch();
+//}
+
+const IndexPage = () => {
+  return (
+    <div>
+      Boom!
+      <AmplifySignOut />
+    </div>
+  );
+};
+
+export default withAuthenticator(IndexPage);
